4973e399f030b9497f33504dc4dcd44f671bf37d
Grant Wallace <gwallace@princeton.edu>
2020-10-27 13:42:48 -0400

    Add verbose flag to sample project script

--- projects/sample/sample.py
+++ projects/sample/sample.py
@@ -16,29 +16,39 @@ Finally, this script is called from 'projectMain.py', which is called from
 
 -----------------------------------------------------------------------------"""
 
-# print a short introduction on the internet window
-print(""
-    "-----------------------------------------------------------------------------\n"
-    "The purpose of this sample project is to demonstrate different ways you can\n"
-    "implement functions, structures, etc. that we have developed for your use.\n"
-    "You will find some comments printed on this html browser. However, if you want\n"
-    "more information about how things work please talk a look at ‘sample.py’.\n"
-    "Good luck!\n"
-    "-----------------------------------------------------------------------------")
+verbose = True
+
+if verbose:
+    # print a short introduction on the internet window
+    print(""
+        "-----------------------------------------------------------------------------\n"
+        "The purpose of this sample project is to demonstrate different ways you can\n"
+        "implement functions, structures, etc. that we have developed for your use.\n"
+        "You will find some comments printed on this html browser. However, if you want\n"
+        "more information about how things work please talk a look at ‘sample.py’.\n"
+        "Good luck!\n"
+        "-----------------------------------------------------------------------------")
 
 # import important modules
 import os
 import sys
 import argparse
+import warnings
 import numpy as np
 import nibabel as nib
 import scipy.io as sio
 
-print(''
-    '|||||||||||||||||||||||||||| IGNORE THIS WARNING ||||||||||||||||||||||||||||')
-from nibabel.nicom import dicomreaders
-print(''
-    '|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')
+if verbose:
+    print(''
+        '|||||||||||||||||||||||||||| IGNORE THIS WARNING ||||||||||||||||||||||||||||')
+with warnings.catch_warnings():
+    if not verbose:
+        warnings.filterwarnings("ignore", category=UserWarning)
+    from nibabel.nicom import dicomreaders
+
+if verbose:
+    print(''
+        '|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')
 
 # obtain full path for current directory: '.../rt-cloud/projects/sample'
 currPath = os.path.dirname(os.path.realpath(__file__))
@@ -93,11 +103,12 @@ def doRuns(cfg, fileInterface, projectComm):
     #       [1] allowedFileTypes (list of allowed file types)
 
     allowedFileTypes = fileInterface.allowedFileTypes()
-    print(""
-    "-----------------------------------------------------------------------------\n"
-    "Before continuing, we need to make sure that dicoms are allowed. To verify\n"
-    "this, use the 'allowedFileTypes'.\n"
-    "Allowed file types: %s" %allowedFileTypes)
+    if verbose:
+        print(""
+        "-----------------------------------------------------------------------------\n"
+        "Before continuing, we need to make sure that dicoms are allowed. To verify\n"
+        "this, use the 'allowedFileTypes'.\n"
+        "Allowed file types: %s" %allowedFileTypes)
 
     # obtain the path for the directory where the subject's dicoms live
     if cfg.isSynthetic:
@@ -105,8 +116,9 @@ def doRuns(cfg, fileInterface, projectComm):
     else:
         subj_imgDir = "{}.{}.{}".format(cfg.datestr, cfg.subjectName, cfg.subjectName)
         cfg.dicomDir = os.path.join(cfg.imgDir, subj_imgDir)
-    print("Location of the subject's dicoms: \n%s\n" %cfg.dicomDir,
-    "-----------------------------------------------------------------------------")
+    if verbose:
+        print("Location of the subject's dicoms: \n%s\n" %cfg.dicomDir,
+        "-----------------------------------------------------------------------------")
 
     # initialize a watch for the entire dicom folder (it doesn't look for a
     #   specific dicom) using the function 'initWatch' in 'fileClient.py'
@@ -115,7 +127,8 @@ def doRuns(cfg, fileInterface, projectComm):
     #       [2] cfg.dicomNamePattern (the naming pattern of dicom files)
     #       [3] cfg.minExpectedDicomSize (a check on size to make sure we don't
     #               accidentally grab a dicom before it's fully acquired)
-    print("• initalize a watch for the dicoms using 'initWatch'")
+    if verbose:
+        print("• initalize a watch for the dicoms using 'initWatch'")
     fileInterface.initWatch(cfg.dicomDir, cfg.dicomNamePattern,
         cfg.minExpectedDicomSize)
 
@@ -130,28 +143,30 @@ def doRuns(cfg, fileInterface, projectComm):
     #       ** the inputs MUST be python integers; it won't work if it's a numpy int
     #
     # here, we are clearing an already existing plot
-    print("• clear any pre-existing plot using 'sendResultToWeb'")
+    if verbose:
+        print("• clear any pre-existing plot using 'sendResultToWeb'")
     projUtils.sendResultToWeb(projectComm, runNum, None, None)
 
-    print(""
-    "-----------------------------------------------------------------------------\n"
-    "In this sample project, we will retrieve the dicom file for a given TR and\n"
-    "then convert the dicom file to a nifti object. **IMPORTANT: In this sample\n"
-    "we won't care about the exact location of voxel data (we're only going to\n"
-    "indiscriminately get the average activation value for all voxels). This\n"
-    "actually isn't something you want to actually do but we'll go through the\n"
-    "to get the data in the appropriate nifti format in the 'advanced sample\n"
-    "project.** We are doing things in this way because it is the simplest way\n"
-    "we can highlight the functionality of rt-cloud, which is the purpose of\n"
-    "this sample project.\n"
-    ".............................................................................\n"
-    "NOTE: We will use the function 'readRetryDicomFromFileInterface' to retrieve\n"
-    "specific dicom files from the subject's dicom folder. This function calls\n"
-    "'fileInterface.watchFile' to look for the next dicom from the scanner.\n"
-    "Since we're using previously collected dicom data, this is functionality is\n"
-    "not particularly relevant for this sample project but it is very important\n"
-    "when running real-time experiments.\n"
-    "-----------------------------------------------------------------------------\n")
+    if verbose:
+        print(""
+        "-----------------------------------------------------------------------------\n"
+        "In this sample project, we will retrieve the dicom file for a given TR and\n"
+        "then convert the dicom file to a nifti object. **IMPORTANT: In this sample\n"
+        "we won't care about the exact location of voxel data (we're only going to\n"
+        "indiscriminately get the average activation value for all voxels). This\n"
+        "actually isn't something you want to actually do but we'll go through the\n"
+        "to get the data in the appropriate nifti format in the 'advanced sample\n"
+        "project.** We are doing things in this way because it is the simplest way\n"
+        "we can highlight the functionality of rt-cloud, which is the purpose of\n"
+        "this sample project.\n"
+        ".............................................................................\n"
+        "NOTE: We will use the function 'readRetryDicomFromFileInterface' to retrieve\n"
+        "specific dicom files from the subject's dicom folder. This function calls\n"
+        "'fileInterface.watchFile' to look for the next dicom from the scanner.\n"
+        "Since we're using previously collected dicom data, this is functionality is\n"
+        "not particularly relevant for this sample project but it is very important\n"
+        "when running real-time experiments.\n"
+        "-----------------------------------------------------------------------------\n")
 
     num_total_TRs = 10  # number of TRs to use for example 1
     if cfg.isSynthetic:
@@ -183,8 +198,10 @@ def doRuns(cfg, fileInterface, projectComm):
         #       [3] timeout (time spent waiting for a file before timing out)
         #   OUTPUT:
         #       [1] dicomData (with class 'pydicom.dataset.FileDataset')
-        print("• use 'readRetryDicomFromFileInterface' to read dicom file for",
-            "TR %d, %s" %(this_TR, fileName))
+        print(f'Processing TR {this_TR}')
+        if verbose:
+            print("• use 'readRetryDicomFromFileInterface' to read dicom file for",
+                "TR %d, %s" %(this_TR, fileName))
         dicomData = readRetryDicomFromFileInterface(fileInterface, fileName,
             timeout_file)
 
@@ -195,20 +212,23 @@ def doRuns(cfg, fileInterface, projectComm):
             #   object. additional steps need to be taken to get the nifti object in
             #   the correct orientation, but we will ignore those steps here. refer to
             #   the 'advanced sample project' for more info about that
-            print("| convert dicom data into a nifti object")
+            if verbose:
+                print("| convert dicom data into a nifti object")
             niftiObject = dicomreaders.mosaic_to_nii(dicomData)
 
         # take the average of all the activation values
-        avg_niftiData = np.mean(niftiObject.get_data())
+        avg_niftiData = np.mean(niftiObject.get_fdata())
         # avg_niftiData = np.round(avg_niftiData,decimals=2)
         print("| average activation value for TR %d is %f" %(this_TR, avg_niftiData))
 
-        max_niftiData = np.amax(niftiObject.get_data())
-        print("| max activation value for TR %d is %d" %(this_TR, max_niftiData))
+        max_niftiData = np.amax(niftiObject.get_fdata())
+        if verbose:
+            print("| max activation value for TR %d is %d" %(this_TR, max_niftiData))
 
         # use 'sendResultToWeb' from 'projectUtils.py' to send the result to the
         #   web browser to be plotted in the --Data Plots-- tab.
-        print("| send result to the web, plotted in the 'Data Plots' tab")
+        if verbose:
+            print("| send result to the web, plotted in the 'Data Plots' tab")
         projUtils.sendResultToWeb(projectComm, runNum, int(this_TR), float(avg_niftiData))
 
         # save the activations value info into a vector that can be saved later
@@ -223,18 +243,21 @@ def doRuns(cfg, fileInterface, projectComm):
     #   INPUT:
     #       [1] filename (full path!)
     #       [2] data (that you want to write into the file)
-    print(""
-    "-----------------------------------------------------------------------------\n"
-    "• save activation value as a text file to tmp folder")
+    if verbose:
+        print(""
+        "-----------------------------------------------------------------------------\n"
+        "• save activation value as a text file to tmp folder")
     fileInterface.putTextFile(output_textFilename,str(all_avg_activations))
 
     # use sio.save mat from scipy to save the matlab file
-    print("• save activation value as a matlab file to tmp folder")
+    if verbose:
+        print("• save activation value as a matlab file to tmp folder")
     sio.savemat(output_matFilename,{'value':all_avg_activations})
 
-    print(""
-    "-----------------------------------------------------------------------------\n"
-    "REAL-TIME EXPERIMENT COMPLETE!")
+    if verbose:
+        print(""
+        "-----------------------------------------------------------------------------\n"
+        "REAL-TIME EXPERIMENT COMPLETE!")
 
     return
 
