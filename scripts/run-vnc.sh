7f3427b34f2d95d806e1f860957188833605696b
Grant Wallace <gwallace@princeton.edu>
2022-01-19 14:02:02 -0500

    Update run-vnc.sh

--- scripts/run-vnc.sh
+++ scripts/run-vnc.sh
@@ -5,6 +5,6 @@ conda activate websockify
 
 vncserver :1 -localhost -SecurityTypes None &
 
-PYTHONPATH="webInterface" websockify --ssl-only --cert certs/rtAtten.crt --key certs/rtAtten_private.key --auth-plugin websockifyAuth.CookieAuth --auth-source certs/cookie-secret 6080 localhost:5901
+PYTHONPATH="web" websockify --ssl-only --cert certs/rtcloud.crt --key certs/rtcloud_private.key --auth-plugin websockifyAuth.CookieAuth --auth-source certs/cookie-secret 6080 localhost:5901
 
 vncserver -kill :1
