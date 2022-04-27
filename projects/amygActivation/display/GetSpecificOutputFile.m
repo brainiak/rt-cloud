5fd67e57ac29de1f76ecfffd5f4871602cfad973
Grant Wallace <gwallace@princeton.edu>
2022-04-27 17:40:18 -0400

    remove projects not used in tests

--- projects/amygActivation/display/GetSpecificOutputFile.m
+++
@@ -1,9 +0,0 @@
-function [fileAvail, specificFile] = GetSpecificOutputFile(classOutputDir,runId,trId)
-
-% update this if you're using a different name
-specificFile = ['percentChange_' runId '_' trId '.txt'];
-if exist(fullfile(classOutputDir,specificFile),'file')
-    fileAvail = 1;
-else
-    fileAvail = 0;
-end
